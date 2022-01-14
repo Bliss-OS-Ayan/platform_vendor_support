ab19079a596e1bfce1e2e6d9ecefbc0084dee028
Pranav Vashi <neobuddy89@gmail.com>
2022-01-14 22:36:20 +0100

    support: Add color picker preference for settings

--- src/com/bliss/support/colorpicker/SystemSettingColorPickerPreference.java
+++ src/com/bliss/support/colorpicker/SystemSettingColorPickerPreference.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.bliss.support.colorpicker;
 
 import android.content.Context;
